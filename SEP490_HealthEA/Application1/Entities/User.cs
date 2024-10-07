e2f216bf4b73acb3622d78e7f92ff32f34d38a85
Manh Vu <manhvv15@gmail.com>
2024-10-08 00:26:58 +0700

    entity user

---
+++ SEP490_HealthEA/Application1/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+
+namespace Application.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public DateTime? Dob { get; set; }
+        public bool? Gender { get; set; }
+        public Guid RoleId { get; set; }
+        public virtual Role Role { get; set; }
+        public string Status { get; set; }
+    }
+}
