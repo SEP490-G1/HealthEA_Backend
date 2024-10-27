e026481eb21746bb1700b5298d2c4dfa689a4644
LongCT_ <thanhlong2704031@gmail.com>
2024-10-27 17:22:25 +0700

    Added Repository

---
+++ SEP490_HealthEA/Domain/Interfaces/IRepositories/IDailyMetricRepository.cs
@@ -0,0 +1,18 @@
+using Domain.Models.Entities.YourNamespace.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.IRepositories
+{
+	public interface IDailyMetricRepository
+	{
+		Task<DailyMetric?> GetByIdAsync(Guid id);
+		Task<IEnumerable<DailyMetric>> GetAllByUserIdAsync(Guid userId);
+		Task AddAsync(DailyMetric dailyMetric);
+		Task UpdateAsync(DailyMetric dailyMetric);
+		Task DeleteAsync(Guid id);
+	}
+}
---
+++ SEP490_HealthEA/Infrastructure/Repositories/DailyMetricRepository.cs
@@ -0,0 +1,58 @@
+using Domain.Interfaces.IRepositories;
+using Domain.Models.Entities.YourNamespace.Models;
+using Infrastructure.SQLServer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+	public class DailyMetricRepository : IDailyMetricRepository
+	{
+		private readonly SqlDBContext _context;
+
+		public DailyMetricRepository(SqlDBContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<DailyMetric?> GetByIdAsync(Guid id)
+		{
+			return await _context.DailyMetrics
+				.Include(dm => dm.User)
+				.FirstOrDefaultAsync(dm => dm.Id == id);
+		}
+
+		public async Task<IEnumerable<DailyMetric>> GetAllByUserIdAsync(Guid userId)
+		{
+			return await _context.DailyMetrics
+				.Where(dm => dm.UserId == userId)
+				.ToListAsync();
+		}
+
+		public async Task AddAsync(DailyMetric dailyMetric)
+		{
+			await _context.DailyMetrics.AddAsync(dailyMetric);
+			await _context.SaveChangesAsync();
+		}
+
+		public async Task UpdateAsync(DailyMetric dailyMetric)
+		{
+			_context.DailyMetrics.Update(dailyMetric);
+			await _context.SaveChangesAsync();
+		}
+
+		public async Task DeleteAsync(Guid id)
+		{
+			var dailyMetric = await GetByIdAsync(id);
+			if (dailyMetric != null)
+			{
+				_context.DailyMetrics.Remove(dailyMetric);
+				await _context.SaveChangesAsync();
+			}
+		}
+	}
+}
