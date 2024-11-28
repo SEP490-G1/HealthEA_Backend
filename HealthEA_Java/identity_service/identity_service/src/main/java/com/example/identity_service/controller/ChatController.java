33b3adb1beecafaab122ed583ace038a313fe7fd
DuongNT <duongnthn2312@gmail.com>
2024-11-28 09:56:58 +0700

    full ai serve

--- HealthEA_Java/identity_service/identity_service/src/main/java/com/example/identity_service/controller/ChatController.java
+++ HealthEA_Java/identity_service/identity_service/src/main/java/com/example/identity_service/controller/ChatController.java
@@ -2,6 +2,8 @@ package com.example.identity_service.controller;
 
 import com.example.identity_service.dto.reponse.DailyMetricsResponse;
 import com.example.identity_service.dto.request.ApiResponse;
+import com.example.identity_service.entity.ChatMessage;
+import com.example.identity_service.enums.SenderType;
 import com.example.identity_service.service.ChatService;
 import com.example.identity_service.service.DailyMetricsService;
 import lombok.AccessLevel;
@@ -13,10 +15,11 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @CrossOrigin(origins = "http://localhost:5173/")
 @RestController
-@RequestMapping("/dailymetrics")
-@RequiredArgsConstructor
+@RequestMapping("/chat")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Slf4j
 
@@ -32,22 +35,32 @@ public class ChatController {
         this.dailyMetricsService = dailyMetricsService;
     }
 
-    @MessageMapping("/sendMessage")
-    public void handleMessage(String userId) throws Exception {
-        // Truy xuất thông tin metrics gần nhất
-        var latestMetrics = dailyMetricsService.getLatestDailyMetrics(userId);
+//    @MessageMapping("/sendMessage")
+//    public void handleMessage(String userId) throws Exception {
+//        // Truy xuất thông tin metrics gần nhất
+//        var latestMetrics = dailyMetricsService.getLatestDailyMetrics(userId);
+//
+//        // Lấy lời khuyên từ AI dựa trên dữ liệu metrics
+//        String advice = chatService.getDailyMetricsAdvice(latestMetrics, "Tinh trang suc khoe dang nhu vao", "vi");
+//
+//        // Gửi lời khuyên tới client qua WebSocket
+//        messagingTemplate.convertAndSend("/topic/public", advice);
+//    }
 
-        // Lấy lời khuyên từ AI dựa trên dữ liệu metrics
-        String advice = chatService.getDailyMetricsAdvice(latestMetrics, "Tinh trang suc khoe dang nhu vao", "vi");
-
-        // Gửi lời khuyên tới client qua WebSocket
-        messagingTemplate.convertAndSend("/topic/public", advice);
+    @PostMapping("/getAiResponse")
+    public ApiResponse<String> sendQuestion(@RequestBody String question) throws Exception {
+        chatService.saveMessage(question, SenderType.USER);
+        String aiRsp = chatService.getDailyMetricsAdvice(dailyMetricsService.getLatestDailyMetrics(), question, "vi");
+        chatService.saveMessage(aiRsp, SenderType.AI);
+        return ApiResponse.<String>builder()
+                .result(aiRsp)
+                .build();
     }
 
-    @PostMapping("/getAiResponse/{userId}")
-    public ApiResponse<String> sendVerificationLink(@PathVariable String userId) throws Exception {
-        return ApiResponse.<String>builder()
-                .result(chatService.getDailyMetricsAdvice(dailyMetricsService.getLatestDailyMetrics(userId), "Tinh trang suc khoe dang nhu vao", "vi"))
+    @GetMapping()
+    public ApiResponse<List<ChatMessage>> getMessagesByUserId() {
+        return ApiResponse.<List<ChatMessage>>builder()
+                .result(chatService.getMessagesByUserId())
                 .build();
     }
 }
--- HealthEA_Java/identity_service/identity_service/src/main/java/com/example/identity_service/service/DailyMetricsService.java
+++ HealthEA_Java/identity_service/identity_service/src/main/java/com/example/identity_service/service/DailyMetricsService.java
@@ -1,6 +1,7 @@
 package com.example.identity_service.service;
 
 import com.example.identity_service.dto.reponse.DailyMetricsResponse;
+import com.example.identity_service.dto.reponse.UserResponse;
 import com.example.identity_service.repository.DailyMetricsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,12 +12,17 @@ import java.util.UUID;
 public class DailyMetricsService {
     private final DailyMetricsRepository dailyMetricsRepository;
 
+    private final UserService userService;
+
     @Autowired
-    public DailyMetricsService(DailyMetricsRepository dailyMetricsRepository) {
+    public DailyMetricsService(DailyMetricsRepository dailyMetricsRepository, UserService userService) {
         this.dailyMetricsRepository = dailyMetricsRepository;
+        this.userService = userService;
     }
 
-    public DailyMetricsResponse getLatestDailyMetrics(String userId) {
+    public DailyMetricsResponse getLatestDailyMetrics() {
+        UserResponse userResponse = userService.getMyInfo();
+        String userId = userResponse.getId();
         DailyMetricsResponse dailyMetricsResponse = new DailyMetricsResponse(userId, dailyMetricsRepository.findMaxWeightByUserId(userId),
                 dailyMetricsRepository.findMaxHeightByUserId(userId),
                 dailyMetricsRepository.findMaxSystolicBloodPressureByUserId(userId),
