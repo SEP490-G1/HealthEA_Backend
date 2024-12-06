e620815828829540170be560eadd214057ebea53
LongCT_ <thanhlong2704031@gmail.com>
2024-12-07 00:11:43 +0700

    Updated UserReport to use background worker for email

--- SEP490_HealthEA/API/Controllers/Customer/UserReportController.cs
+++ SEP490_HealthEA/API/Controllers/Customer/UserReportController.cs
@@ -5,6 +5,7 @@ using Domain.Models.DAO.Doctor;
 using Domain.Models.Entities;
 using Infrastructure.Repositories;
 using Infrastructure.Services;
+using Infrastructure.Services.Background;
 using Infrastructure.SQLServer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -21,12 +22,13 @@ namespace API.Controllers.Customer
 		private readonly IMapper mapper;
 		private readonly IUserClaimsService userClaimsService;
 		private readonly EmailService emailService;
+		private readonly IBackgroundTaskQueue queue;
 
 		private readonly IDoctorRepository doctorRepository;
 		private readonly INewsRepository newsRepository;
 		private readonly IUserRepository userRepository;
 
-		public UserReportController(IMapper mapper, IUserReportRepository repository, IUserClaimsService userClaimsService, IDoctorRepository doctorRepository, INewsRepository newsRepository, EmailService emailService, IUserRepository userRepository)
+		public UserReportController(IMapper mapper, IUserReportRepository repository, IUserClaimsService userClaimsService, IDoctorRepository doctorRepository, INewsRepository newsRepository, EmailService emailService, IUserRepository userRepository, IBackgroundTaskQueue queue)
 		{
 			this.mapper = mapper;
 			this.repository = repository;
@@ -35,6 +37,7 @@ namespace API.Controllers.Customer
 			this.newsRepository = newsRepository;
 			this.emailService = emailService;
 			this.userRepository = userRepository;
+			this.queue = queue;
 		}
 
 		[HttpGet("{id}")]
@@ -75,19 +78,20 @@ namespace API.Controllers.Customer
 			report.CreatedAt = DateTime.UtcNow;
 			await repository.AddUserReportAsync(report);
 			//Send email to user
-			var user = await userRepository.GetUserByIdAsync(report.ReporterId);
-			if (user != null)
+			queue.QueueBackgroundWorkItem(async (provider, token) =>
 			{
-				var thread = new Thread(async () =>
+				var userRepository = provider.GetRequiredService<IUserRepository>();
+				var emailService = provider.GetRequiredService<EmailService>();
+				var user = await userRepository.GetUserByIdAsync(report.ReporterId);
+				if (user != null)
 				{
 					await emailService.SendEmailAsync(user.Email,
 					"Chúng tôi đã nhận được báo cáo của bạn",
 					$"<h2>Xin chào {user.Username}.</h2>" +
 					"<p>Chúng tôi đã nhận được báo cáo của bạn và sẽ tiến hành xem xét trong thời gian sớm nhất.</p>" +
 					"<p>Cảm ơn bạn đã giúp chúng tôi cải thiện hệ thống.</p>");
-				});
-				thread.Start();
-			}
+				}
+			});
 			return CreatedAtAction(nameof(GetReportById), new { id = report.Id }, report);
 		}
 
@@ -107,22 +111,23 @@ namespace API.Controllers.Customer
 			await repository.MarkReportStatusAsync(id, model.Status);
 			if (model.Status == 1)
 			{
-				UserReport report = await repository.GetReportByIdAsync(id);
+				var report = await repository.GetReportByIdAsync(id);
 				if (report != null)
 				{
-					var user = await userRepository.GetUserByIdAsync(report.ReporterId);
-					if (user != null)
+					queue.QueueBackgroundWorkItem(async (provider, token) =>
 					{
-						var thread = new Thread(async () =>
+						var userRepository = provider.GetRequiredService<IUserRepository>();
+						var emailService = provider.GetRequiredService<EmailService>();
+						var user = await userRepository.GetUserByIdAsync(report.ReporterId);
+						if (user != null)
 						{
 							await emailService.SendEmailAsync(user.Email,
 							"Báo cáo của bạn đã được giải quyết",
 							$"<h2>Xin chào {user.Username}.</h2>" +
 							"<p>Chúng tôi đã xem xét báo cáo của bạn và đã xử lý vấn đề. Nếu có thắc mắc hoặc cần hỗ trợ thêm, vui lòng liên hệ lại với chúng tôi.</p>" +
 							"<p>Cảm ơn bạn đã giúp chúng tôi cải thiện hệ thống.</p>");
-						});
-						thread.Start();
-					}
+						}
+					});
 				}
 			}
 			return NoContent();
